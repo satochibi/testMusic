8722007ccd8af372235c4db4903777836024f185
satochibi <satochibi0120@gmail.com>
2021-08-28 14:08:22 +0900

    AudiosetとMoveをMoveに統合、ゲームスタートボタンを実装

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
     //ノーツを移動させるスクリプト
-    public float speed = 10f;
-    private Rigidbody rb;
+    [SerializeField]
+    float speed = 10f;
+
+    [SerializeField]
+    GameObject AudioObject;
+
+    public float Speed { get { return this.speed; } }
+
+
+    AudioClip clip;
+    Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        clip = AudioObject.GetComponent<AudioSource>().clip;
+    }
+
+    public void GameStart(GameObject sender)
+    {
+        sender.SetActive(false);
         rb.AddForce(transform.forward * -speed, ForceMode.VelocityChange);
+        AudioObject.SetActive(true);
     }
 
 }
