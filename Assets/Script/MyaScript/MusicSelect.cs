690c0c6bab713b588003b17bcf155b212e44dad3
BuuMya <buumya1324@gmail.com>
2021-09-06 14:33:21 +0900

    Jsonファイルの名前一部変更 Resouceクラスでのファイル読み込みに統一

--- Assets/Script/MyaScript/MusicSelect.cs
+++ Assets/Script/MyaScript/MusicSelect.cs
@@ -13,7 +13,8 @@ public class MusicSelect : MonoBehaviour
     public GameObject listItemPre;
     public List<GameObject> m_nameList;
     public GameObject contentOBJ;
-    
+
+    public List<Humen> musicList;
     Vector3 pos;
    
     public void EraceList()
@@ -49,14 +50,20 @@ public class MusicSelect : MonoBehaviour
         Time.timeScale = 1.0f;
         //指定フォルダからjsonファイルを一括読み込み
        // string path = Application.dataPath + "/Resources/NoteJson/";
-             names = Directory.GetFiles(@path, "*.json",SearchOption.TopDirectoryOnly);
-        
-        Debug.Log(names[0]);
+             //names = Directory.GetFiles(@path, "*.json",SearchOption.TopDirectoryOnly);
+        TextAsset[] textassets =Resources.LoadAll<TextAsset>("NoteJson");
+        Array.Resize<string>(ref names, textassets.Length);
+        for(int i =0;i<textassets.Length;i++)
+        {
+            musicList.Add(JsonUtility.FromJson<Humen>(textassets[i].ToString()));
+            names[i] = musicList[i].name;
+        }
+        Debug.Log(textassets);
   
         for (int i = 0; i < names.Length; i++)
         {
 
-            names[i] = Path.GetFileNameWithoutExtension(names[i]);
+            //names[i] = Path.GetFileNameWithoutExtension(names[i]);
             GameObject m_obj = Instantiate(listItemPre, pos, Quaternion.identity);
             //m_obj.transform.parent = contentOBJ.transform;
             m_obj.transform.SetParent(contentOBJ.transform, false);
