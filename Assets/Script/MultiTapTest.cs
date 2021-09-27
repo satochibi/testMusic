b89db013b5a177e8e5712f8be36c184566e30344
BuuMya <buumya1324@gmail.com>
2021-09-27 15:19:19 +0900

    マルチタップテスト

--- Assets/Script/MultiTapTest.cs
+++ Assets/Script/MultiTapTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,46 +7,79 @@ public class MultiTapTest : MonoBehaviour
 {
     [SerializeField]
     GameObject tapTextGameObj;
-    
+
 
     Text tapText;
-    
+    List<Touch> touches = new List<Touch>();
+    List<string> messages = new List<string>();
+
     void Start()
     {
         tapText = tapTextGameObj.GetComponent<Text>();
-        
+
     }
     void Update()
     {
         // Track a single touch as a direction control.
-        
+        //タッチ取得
         var touchCount = Input.touchCount;
+
+        if (touchCount == 0)
+        {
+            touches.Clear();
+            messages.Clear();
+        }
+
         for (int i = 0; i < touchCount; i++)
         {
-            Touch touch = Input.GetTouch(i);
+            touches.Add(Input.GetTouch(i));
+        }
 
+        //タッチ情報格納
+
+        for (int i = 0; i < touches.ToArray().Length; i++)
+        {
+            Touch touch = touches[i];
+            string aMessage = "";
             // Handle finger movements based on touch phase.
             switch (touch.phase)
             {
                 // Record initial touch position.
                 case TouchPhase.Began:
-                    tapText.text = "開始地点"+touch.position.ToString();
-                    Debug.Log("Began"+touch.position);
+                    aMessage = "開始地点: " + touch.position.ToString() + Environment.NewLine;
+                    //Debug.Log("Began" + touch.position);
                     break;
 
                 // Determine direction by comparing the current touch position with the initial one.
                 case TouchPhase.Moved:
-                    tapText.text ="移動中"+ touch.position.ToString();
-                    Debug.Log("Moved"+touch.position);
+                    aMessage = "移動中" + touch.position.ToString() + Environment.NewLine;
+                    //Debug.Log("Moved" + touch.position);
                     break;
 
                 // Report that a direction has been chosen when the finger is lifted.
                 case TouchPhase.Ended:
-                    tapText.text = "離した"+touch.position.ToString();
-                    Debug.Log("Ended"+touch.position);
+                    //aMessage = "離した" + touch.position.ToString() + Environment.NewLine;
+                    touches.Remove(touch);
+                    messages.RemoveAt(touchCount - 1);
+
+                    //Debug.Log("Ended" + touch.position);
                     break;
             }
+
+            messages.Add(aMessage);
         }
+
+        //メッセージ表示
+        string resultMessage = "";
+        foreach(string message in messages)
+        {
+            resultMessage += message;
+        }
+        tapText.text = resultMessage;
         
+
+
+
     }
+
 }
