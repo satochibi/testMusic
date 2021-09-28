a588a44aeba69a108b908dcd8d0b00569033013f
BuuMya <buumya1324@gmail.com>
2021-09-28 11:41:51 +0900

    マルチタップ実験中

--- Assets/Script/MultiTapTest.cs
+++ Assets/Script/MultiTapTest.cs
@@ -22,93 +22,142 @@ public class MultiTapTest : MonoBehaviour
         //    Debug.Log(hit.collider.gameObject.transform.position);
         //}
     }
+    public void MousePointerRaycastTest()
+    {
+        Ray mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit mhit = default;
+        if (Physics.Raycast(mouseray, out mhit, 40.0f))
+        {
+            mhit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+
+            Debug.Log(mhit.collider.gameObject.transform.position + Environment.NewLine);
+            Debug.Log("マウス座標"+Input.mousePosition.ToString());
+        }
+        
+    }
+
+    public void SingleTouchTest()
+    {
+        Touch touch = Input.GetTouch(0);
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 40.0f))
+        {
+            hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+
+            Debug.Log(hit.collider.gameObject.transform.position);
+        }
+
+        switch (touch.phase)
+        {
+
+            // Record initial touch position.
+            case TouchPhase.Began:
+                tapText.text = "開始地点" + touch.position.ToString() + Environment.NewLine;
+                //Debug.Log("Began" + touch.position);
+                break;
+
+            // Determine direction by comparing the current touch position with the initial one.
+            case TouchPhase.Moved:
+                tapText.text = "移動中" + touch.position.ToString() + Environment.NewLine;
+                //Debug.Log("Moved" + touch.position);
+                break;
+
+            // Report that a direction has been chosen when the finger is lifted.
+            case TouchPhase.Ended:
+                tapText.text = "離した" + touch.position.ToString() + Environment.NewLine;
+
+                break;
+        }
+    }
     void Update()
     {
         // Track a single touch as a direction control.
         //タッチ取得
         var touchCount = Input.touchCount;
 
+        
+        if (touchCount > 0)
+        {
+            SingleTouchTest();
+        }
+
+        MousePointerRaycastTest();
+
+
         if (touchCount == 0)
         {
             touches.Clear();
             messages.Clear();
         }
-        //Ray mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit mhit =default;
-        //if (Physics.Raycast(mouseray, out mhit, 40.0f))
-        //{
-        //    mhit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
 
-        //    Debug.Log(mhit.collider.gameObject.transform.position);
-        //}
         for (int i = 0; i < touchCount; i++)
         {
             touches.Add(Input.GetTouch(i));
-            Ray ray = Camera.main.ScreenPointToRay(touches[i].position);
-            rays.Add(ray);
-            //if (Physics.Raycast(ray, out hit, 40.0f))
-            //{
-            //    hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
-            //    //Debug.Log(hit.collider.gameObject.transform.position);
-            //    rays.Add(ray);
-            //}
-                
+
+
         }
 
+        //if (Physics.Raycast(ray, out hit, 40.0f))
+        //{
+        //    hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+        //    //Debug.Log(hit.collider.gameObject.transform.position);
+        //    rays.Add(ray);
+        //}
         //タッチ情報格納
-        
-        for (int i = 0; i < touches.ToArray().Length; i++)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(rays[i], out hit, 40.0f))
-            {
-                hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
-                
-            }
-            Touch touch = touches[i];
-            string aMessage = "";
-            // Handle finger movements based on touch phase.
-            switch (touch.phase)
-            {
 
-                // Record initial touch position.
-                case TouchPhase.Began:
-                    aMessage = "開始地点: " + touch.position.ToString() + Environment.NewLine;
-                    //Debug.Log("Began" + touch.position);
-                    break;
+        //for (int i = 0; i < touches.ToArray().Length; i++)
+        //{
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(rays[i], out hit, 40.0f))
+        //    {
+        //        hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
 
-                // Determine direction by comparing the current touch position with the initial one.
-                case TouchPhase.Moved:
-                    aMessage = "移動中" + touch.position.ToString() + Environment.NewLine;
-                    //Debug.Log("Moved" + touch.position);
-                    break;
+        //    }
+        //    Touch touch = touches[i];
+        //    string aMessage = "";
+        //    // Handle finger movements based on touch phase.
+        //    switch (touch.phase)
+        //    {
 
-                // Report that a direction has been chosen when the finger is lifted.
-                case TouchPhase.Ended:
-                    //aMessage = "離した" + touch.position.ToString() + Environment.NewLine;
-                    int index = touches.FindIndex((anotherTouch) => anotherTouch.fingerId == touch.fingerId);
-                    //touches.RemoveAt(index);
-                    // messages.RemoveAt(index);
-                    tapTextGameObj.transform.Translate(0.0f,2.0f,0.0f);
-                    //rays.RemoveAt(index);
-                    //Debug.Log("Ended" + touch.position);
-                    break;
-            }
+        //        // Record initial touch position.
+        //        case TouchPhase.Began:
+        //            aMessage = "開始地点: " + touch.position.ToString() + Environment.NewLine;
+        //            //Debug.Log("Began" + touch.position);
+        //            break;
 
-            messages.Add(aMessage);
-        }
+        //        // Determine direction by comparing the current touch position with the initial one.
+        //        case TouchPhase.Moved:
+        //            aMessage = "移動中" + touch.position.ToString() + Environment.NewLine;
+        //            //Debug.Log("Moved" + touch.position);
+        //            break;
 
-        //メッセージ表示
-        string resultMessage = "";
-        foreach(string message in messages)
-        {
-            resultMessage += message;
-        }
-        tapText.text = resultMessage;
-        
+        //        // Report that a direction has been chosen when the finger is lifted.
+        //        case TouchPhase.Ended:
+        //            //aMessage = "離した" + touch.position.ToString() + Environment.NewLine;
+        //            int index = touches.FindIndex((anotherTouch) => anotherTouch.fingerId == touch.fingerId);
+        //            //touches.RemoveAt(index);
+        //            // messages.RemoveAt(index);
+        //            tapTextGameObj.transform.Translate(0.0f,2.0f,0.0f);
+        //            //rays.RemoveAt(index);
+        //            //Debug.Log("Ended" + touch.position);
+        //            break;
+        //    }
+
+        //    messages.Add(aMessage);
+        //}
+
+        ////メッセージ表示
+        //string resultMessage = "";
+        //foreach(string message in messages)
+        //{
+        //    resultMessage += message;
+        //}
+        //tapText.text = resultMessage;
+
 
 
 
     }
 
-}
+    }
